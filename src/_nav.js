8981a0c0a834012ce687bb7eb3fe0d6da68fe800
abhishek singh <abhishekcvinfotech.com>
2023-10-25 14:41:02 +0530

    navigation corrected

--- src/_nav.js
+++ src/_nav.js
@@ -284,36 +284,78 @@ const _nav = [
         to: '/location',
         icon: <FaBuildingShield className='faiconsdashboard' />,
       },
-
-    ],
-  },
-
-  {
-    component: CNavGroup,
-    name: 'Notifications',
-    icon: <IoNotifications className='faiconsdashboard' />,
-    items: [
       {
-        component: CNavItem,
-        name: 'Splash Screen',
-        to: '/splash',
-        icon: <FaMobileScreenButton className='faiconsdashboard' />,
-      },
-
-      {
-        component: CNavItem,
-        name: 'App Notifications',
-        to: '/notification',
-        icon: <BiSolidMobileVibration className='faiconsdashboard' />,
-      },
+        component: CNavGroup,
+        name: 'Notifications',
+        icon: <IoNotifications className='faiconsdashboard' />,
+        items: [
           {
             component: CNavItem,
-            name: 'Trash',
-            to: '/Trash',
-            icon: <FaTrashCan className='faiconsdashboard' />,
+            name: 'Splash Screen',
+            to: '/splash',
+            icon: <FaMobileScreenButton className='faiconsdashboard' />,
           },
+    
+          {
+            component: CNavItem,
+            name: 'App Notifications',
+            to: '/notification',
+            icon: <BiSolidMobileVibration className='faiconsdashboard' />,
+          },
+              {
+                component: CNavItem,
+                name: 'Trash',
+                to: '/Trash',
+                icon: <FaTrashCan className='faiconsdashboard' />,
+              },
+    
+          // {
+          //   component: CNavItem,
+          //   name: 'Trash',
+          //   to: '/Trash',
+          //   icon: <FaTrashCan className='faiconsdashboard' />,
+          // },
+    
+        ],
+      },
+
     ],
   },
+
+  // {
+  //   component: CNavGroup,
+  //   name: 'Notifications',
+  //   icon: <IoNotifications className='faiconsdashboard' />,
+  //   items: [
+  //     {
+  //       component: CNavItem,
+  //       name: 'Splash Screen',
+  //       to: '/splash',
+  //       icon: <FaMobileScreenButton className='faiconsdashboard' />,
+  //     },
+
+  //     {
+  //       component: CNavItem,
+  //       name: 'App Notifications',
+  //       to: '/notification',
+  //       icon: <BiSolidMobileVibration className='faiconsdashboard' />,
+  //     },
+  //         {
+  //           component: CNavItem,
+  //           name: 'Trash',
+  //           to: '/Trash',
+  //           icon: <FaTrashCan className='faiconsdashboard' />,
+  //         },
+
+  //     // {
+  //     //   component: CNavItem,
+  //     //   name: 'Trash',
+  //     //   to: '/Trash',
+  //     //   icon: <FaTrashCan className='faiconsdashboard' />,
+  //     // },
+
+  //   ],
+  // },
 
   {
     component: CNavItem,
